4243185053ce85ac920d8f359125780e5c12c81c
abhishek <somu8023@gmail.com>
2023-09-19 22:35:42 -0500

    My first commit_0919

---
+++ src/test/java/runNowTest/myFirstTest.java
@@ -0,0 +1,12 @@
+package runNowTest;
+
+import org.testng.annotations.Test;
+
+public class myFirstTest {
+
+    @Test
+    public void firstRun()
+    {
+        System.out.println("My name is Abhishek");
+    }
+}
